8113023d9a32bd476a2920b507524100b27476d7
yuabrahamliu <39322184+yuabrahamliu@users.noreply.github.com>
2022-09-04 19:04:54 -0400

    Delete inst/scripts directory

--- inst/scripts/pairedDNAmdat.R
+++
@@ -1,7 +0,0 @@
-# GSE98224 contains paired RNA and DNA methylation data for 48 human placenta 
-# sampels. Its Infinium 450K DNA methylation data were downloaded from GEO. 
-# Then, SeSAMe was used to perform data preprocessing and only probes appeared 
-# in both the Illumina 27K and 450K platforms were kept.
-#
-# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE98224
-#
