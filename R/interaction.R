c5e9934a5606a897173203470243d45c9ed98fd9
yuabrahamliu <39322184+yuabrahamliu@users.noreply.github.com>
2022-09-04 18:17:33 -0400

    Add files via upload

--- R/interaction.R
+++ R/interaction.R
@@ -893,8 +893,6 @@ geneannotation <- function(genesymbols = NULL,
 
   #summaryannotation <-
   #  readRDS('C:/Users/liuy47/Desktop/Transfer/codestransfer/deconv/files/summaryannotation.rds')
-  
-  data('summaryannotation')
 
   summaryannotation <- get('summaryannotation')
 
